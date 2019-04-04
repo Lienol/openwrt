e25e55dcd11e7ab2a6ef1921aaee3addd457e7eb
李国 <uxgood.org@gmail.com>
2019-04-04 03:42:16 +0000

    x86: add EFI images and make iso images EFI bootable
    Add EFI bootable images for x86 platforms. These images can also
    boot from legacy BIOS. And iso images can boot from EFI now.
    
    EFI System Partition need to be fat12/fat16/fat32 (not need to load
    filesystem drivers), so the first partition of EFI images are not ext4
    filesystem any more.
    
    GPT partition table has an alternate partition table, we did not
    generate it. This may cause problems when use these images as qemu disk,
    kernel can not find rootfs, we pad enough sectors will be ok.
    
    Signed-off-by: 李国 <uxgood.org@gmail.com>
    
    x86: fix sysupgrade for EFI images
    
    1. make function get_partitions support GPT disks
    2. get more bytes from image to get GPT partition table
    3. update the original PARTUUID to new grub.cfg
    
    Signed-off-by: 李国 <uxgood.org@gmail.com>

--- scripts/gen_image_generic.sh
+++ scripts/gen_image_generic.sh
@@ -20,7 +20,7 @@ sect=63
 cyl=$(( ($KERNELSIZE + $ROOTFSSIZE) * 1024 * 1024 / ($head * $sect * 512)))
 
 # create partition table
-set `ptgen -o "$OUTPUT" -h $head -s $sect -p ${KERNELSIZE}m -p ${ROOTFSSIZE}m ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE}`
+set `ptgen -o "$OUTPUT" -h $head -s $sect ${EFI_SIGNATURE:+-g} -p ${KERNELSIZE}m -p ${ROOTFSSIZE}m ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} ${EFI_SIGNATURE:+-G $EFI_SIGNATURE}`
 
 KERNELOFFSET="$(($1 / 512))"
 KERNELSIZE="$2"
@@ -30,6 +30,12 @@ ROOTFSSIZE="$(($4 / 512))"
 [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc count="$ROOTFSSIZE"
 dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
 
-make_ext4fs -J -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
+if [ -n "$EFI_SIGNATURE" ]; then
+    [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$(($ROOTFSOFFSET + $ROOTFSSIZE))" conv=notrunc count="$sect"
+    mkfs.fat -C "$OUTPUT.kernel" -S 512 "$(($KERNELSIZE / 1024))"
+    mcopy -s -i "$OUTPUT.kernel" "$KERNELDIR"/* ::/
+else
+    make_ext4fs -J -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
+fi
 dd if="$OUTPUT.kernel" of="$OUTPUT" bs=512 seek="$KERNELOFFSET" conv=notrunc
 rm -f "$OUTPUT.kernel"
