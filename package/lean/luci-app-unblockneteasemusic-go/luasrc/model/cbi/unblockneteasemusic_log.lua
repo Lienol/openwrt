eceae381c64b313ff5b45d04718639482fdd3853
Lienol <lawlienol@gmail.com>
2020-02-25 20:25:48 +0800

    unblockmusic: sync lean's package
    luci-app-unblockmusic: resize and update
    
    luci-app-unblockmusic: add wget depend
    
    UnblockNeteaseMusic:may can fix memory overflow
    
    luci-app-unblockmusic: add block NeteaseCloudMusic spec ip
    
    luci-app-unblockmusic: fix hijack
    
    luci-app-unblockmusic: fix hotplug.d script
    
    luci-app-unblockmusic: fix update core in low mem (=<128M) router
    
    luci-app-unblockneteasemusic-go: add go version unblockneteasemusic for NO-FPU dev
    
    luci-app-unblockneteasemusic-mini: add package (#3218)
    
    luci-app-unblockneteasemusic-mini: change name
    
    luci-app-unblockneteasemusic-mini: fix typo error (#3231)
    
    luci-app-unblockmusic: enable FLAC unlocked
    
    UnblockNeteaseMusic: bump to 0.24.1 (flac support)
    
    luci-app-unblockneteasemusic-mini: add flac server
    
    luci-app-unblockneteasemusic-mini: fix typo

--- package/lean/luci-app-unblockneteasemusic-go/luasrc/model/cbi/unblockneteasemusic_log.lua
+++ package/lean/luci-app-unblockneteasemusic-go/luasrc/model/cbi/unblockneteasemusic_log.lua
@@ -1,13 +1,12 @@
 local fs = require "nixio.fs"
-local conffile = "/tmp/music.log"
+local conffile = "/tmp/unblockneteasemusic.log"
 
 f = SimpleForm("logview")
 
 t = f:field(TextValue, "conf")
 t.rmempty = true
-t.rows = 20
+t.rows = 15
 function t.cfgvalue()
-  luci.sys.exec("grep -B 1 'http' /tmp/unblockmusic.log | grep -v 'running' > /tmp/music.log")
 	return fs.readfile(conffile) or ""
 end
 t.readonly="readonly"
