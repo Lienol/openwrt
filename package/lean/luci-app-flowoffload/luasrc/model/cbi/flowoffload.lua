1d134c9fdd2050ef7848404818b327eb915204ee
Lienol <lawlienol@gmail.com>
2020-03-13 21:54:00 +0800

    luci-app-flowoffload: optimize

--- package/lean/luci-app-flowoffload/luasrc/model/cbi/flowoffload.lua
+++ package/lean/luci-app-flowoffload/luasrc/model/cbi/flowoffload.lua
@@ -15,25 +15,46 @@ flow.default = 0
 flow.rmempty = false
 flow.description = translate("Enable software flow offloading for connections. (decrease cpu load / increase routing throughput)")
 
+function flow.cfgvalue(...)
+	return m.uci:get("firewall", "@defaults[0]", "flow_offloading") or "0"
+end
+
+function flow.write(self, section, value)
+    m.uci:set("firewall", "@defaults[0]", "flow_offloading", value)
+    m.uci:commit("firewall")
+end
+
 hw = s:option(Flag, "flow_offloading_hw", translate("HWNAT"))
 hw.default = 0
 hw.rmempty = true
 hw.description = translate("Enable Hardware NAT (depends on hw capability like MTK 762x)")
 hw:depends("flow_offloading", 1)
 
+function hw.cfgvalue(...)
+	return m.uci:get("firewall", "@defaults[0]", "flow_offloading_hw") or "0"
+end
+
+function hw.write(self, section, value)
+    m.uci:set("firewall", "@defaults[0]", "flow_offloading_hw", value)
+    m.uci:commit("firewall")
+end
+
+o = s:option(Flag, "fullcone", translate("Enable FullCone-NAT"))
+o.default = 0
+o.rmempty = false
+
+function o.cfgvalue(...)
+	return m.uci:get("firewall", "@defaults[0]", "fullcone") or "0"
+end
+
+function o.write(self, section, value)
+    m.uci:set("firewall", "@defaults[0]", "fullcone", value)
+    m.uci:commit("firewall")
+end
+
 bbr = s:option(Flag, "bbr", translate("Enable BBR"))
 bbr.default = 0
 bbr.rmempty = false
 bbr.description = translate("Bottleneck Bandwidth and Round-trip propagation time (BBR)")
-
-dns = s:option(Flag, "dns", translate("DNS Acceleration"))
-dns.default = 0
-dns.rmempty = false
-dns.description = translate("Enable DNS Cache Acceleration and anti ISP DNS pollution")
-
-o = s:option(Value, "dns_server", translate("Upsteam DNS Server"))
-o.default = "114.114.114.114,114.114.115.115"
-o.description = translate("Muitiple DNS server can saperate with ','")
-o:depends("dns", 1)
 
 return m
