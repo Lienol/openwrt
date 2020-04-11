a251b5832bd34979b19cdac691f5ceb6fc9e2e86
Lienol <lawlienol@gmail.com>
2020-04-11 21:47:18 +0800

    ipq40xx: add support for P&W R619AC
    remove model name from LED labels

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -99,7 +99,9 @@ platform_do_upgrade() {
 		CI_UBIPART="ubifs"
 		askey_do_upgrade "$1"
 		;;
-	compex,wpj419)
+	compex,wpj419|\
+	p2w,r619ac|\
+	p2w,r619ac-128m)
 		nand_do_upgrade "$1"
 		;;
 	linksys,ea6350v3 |\
