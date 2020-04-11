7fc5f80bdd7c617138aba16e3f9a3d8e62761dae
doney <doney_0318@msn.com>
2020-04-11 19:59:12 +0800

    ipq40xx

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -9,10 +9,11 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(macaddr_add "$base_mac" +1)
 		ip link set dev eth1 address $(macaddr_add "$base_mac" +3)
 		;;
-	linksys,ea8300)
-		base_mac=$(mtd_get_mac_ascii devinfo hw_mac_addr)
-		ip link set dev eth0 address "${base_mac}"
-		ip link set dev eth1 address $(macaddr_add "${base_mac}" 1)
+	asus,rt-acrh17|\
+	asus,rt-ac58u)
+		CI_UBIPART=UBI_DEV
+		mac=$(macaddr_add $(mtd_get_mac_binary_ubi Factory 4102) +1)
+		ifconfig eth0 hw ether $mac 2>/dev/null
 		;;
 	meraki,mr33)
 		mac_lan=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 102)
@@ -21,7 +22,7 @@ preinit_set_mac_address() {
 	zyxel,nbg6617)
 		base_mac=$(cat /sys/class/net/eth0/address)
 		ip link set dev eth0 address $(macaddr_add "$base_mac" +2)
-		ip link set dev eth1 address $(macaddr_add "$base_mac" +3)
+		;;
 	esac
 }
 
