9637b3f9cc2cf845e7ce8076e81ce202d63caa2d
Lienol <lawlienol@gmail.com>
2020-04-14 13:59:16 +0800

    feeds: luci: switch to stable 19.07 branch

--- force_pull.sh
+++ force_pull.sh
@@ -2,5 +2,5 @@
 git fetch --all
 git reset --hard origin/dev-master
 cd feeds/packages && git fetch --all && git reset --hard origin/dev-master && cd ../../
-cd feeds/luci && git fetch --all && git reset --hard origin/dev-master && cd ../../
+cd feeds/luci && git fetch --all && git reset --hard origin/dev-19.07 && cd ../../
 ./scripts/feeds update -a && ./scripts/feeds install -a
