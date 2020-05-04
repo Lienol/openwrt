d346f8f82554f49ba311c78f2ea25fa9abc3e341
Lienol <lawlienol@gmail.com>
2020-05-05 02:08:11 +0800

    mac80211: add support for 256-QAM

--- package/network/services/hostapd/files/hostapd.sh
+++ package/network/services/hostapd/files/hostapd.sh
@@ -101,6 +101,7 @@ hostapd_common_add_device_config() {
 	config_add_boolean legacy_rates
 	config_add_int cell_density
 	config_add_int rts_threshold
+	config_add_boolean vendor_vht
 
 	config_add_string acs_chan_bias
 	config_add_array hostapd_options
@@ -117,7 +118,7 @@ hostapd_prepare_device_config() {
 	local base_cfg=
 
 	json_get_vars country country3 country_ie beacon_int:100 dtim_period:2 doth require_mode legacy_rates \
-		acs_chan_bias local_pwr_constraint spectrum_mgmt_required airtime_mode cell_density \
+		acs_chan_bias vendor_vht local_pwr_constraint spectrum_mgmt_required airtime_mode cell_density \
 		rts_threshold beacon_rate
 
 	hostapd_set_log_options base_cfg
@@ -187,6 +188,7 @@ hostapd_prepare_device_config() {
 				set_default rate_list "24000 36000 48000 54000"
 				set_default basic_rate_list "24000"
 			fi
+			[ -n "$vendor_vht" ] && append base_cfg "vendor_vht=$vendor_vht" "$N"
 		;;
 		a)
 			if [ "$cell_density" -eq 1 ]; then
