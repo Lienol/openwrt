3a033f4be63630e458202720d485ec1f5b4fce16
Lienol <lawlienol@gmail.com>
2020-07-05 15:53:11 +0800

    kernel: Update kernel 4.19 to version 4.19.131
    Fixes:
    - CVE-2020-10757
    
    The "mtd: rawnand: Pass a nand_chip object to nand_release()" commit was
    backported which needed some adaptations to other code.
    
    Run tested: ath79
    Build tested: ath79
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .223
 LINUX_VERSION-4.14 = .187
-LINUX_VERSION-4.19 = .123
+LINUX_VERSION-4.19 = .131
 
 LINUX_KERNEL_HASH-4.9.223 = cf5300e6f5d8c66c2bed8f00d53f9c58103731809862427012e4010f5d782ae5
 LINUX_KERNEL_HASH-4.14.187 = 5b223475eaeea196aa7e127d3f253bca5c35d8afdc72ca75230ce1ecdd1454bd
-LINUX_KERNEL_HASH-4.19.123 = a79914d31a8d8c6b0e2bb0f2b143d615fe8a6c4dd2e0f36e97aa20efd69a993f
+LINUX_KERNEL_HASH-4.19.131 = 19dfb9f6cc4ba30104b65dcce7d78240a4ae188cb366747d5f8eae35e98964ba
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
