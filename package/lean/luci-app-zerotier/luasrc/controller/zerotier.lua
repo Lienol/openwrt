0af82175ce078f484cf2eebe03ec0c991d8e7b1b
Li2nOnline <lawlienol@gmail.com>
2020-07-25 14:01:19 +0800

    Update zerotier.lua

--- package/lean/luci-app-zerotier/luasrc/controller/zerotier.lua
+++ package/lean/luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -5,8 +5,6 @@ function index()
 return
 end
 
-entry({"admin","network"}, firstchild(), "network", 45).dependent = false
-
 entry({"admin", "network", "zerotier"},firstchild(), _("ZeroTier")).dependent = false
 
 entry({"admin", "network", "zerotier", "general"},cbi("zerotier/settings"), _("Base Setting"), 1)
