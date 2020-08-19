281f9043b578298a86e59720f90651d1af44bdbc
Li2nOnline <lawlienol@gmail.com>
2020-08-20 01:01:21 +0800

    target.mk: add aliyun and dnspod ddns by default

--- include/target.mk
+++ include/target.mk
@@ -14,7 +14,8 @@ DEVICE_TYPE?=router
 
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd luci luci-compat wget \
-default-settings luci-app-ddns luci-app-wol luci-app-vlmcsd luci-app-ramfree luci-app-sfe \
+default-settings luci-app-wol luci-app-vlmcsd luci-app-ramfree luci-app-sfe \
+luci-app-ddns ddns-scripts_aliyun ddns-scripts_dnspod \
 luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
