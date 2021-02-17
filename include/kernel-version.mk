75bc54df3ffce29b9e53187bfe0420c03b655149
CN_SZTL <22235437+1715173329@users.noreply.github.com>
2021-02-17 15:32:59 +0800

    kernel: bump 4.19 to 4.19.175

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .257
 LINUX_VERSION-4.14 = .221
-LINUX_VERSION-4.19 = .148
+LINUX_VERSION-4.19 = .175
 
 LINUX_KERNEL_HASH-4.9.257 = 95e780fffc81de8217a31a3e6e212b69643f5344e9ba0dabaa6e426126f6d64f
 LINUX_KERNEL_HASH-4.14.221 = ce2254075ff4846f13380c1cf4bec1e351e5996a7dd109c200e222e49fd2c7e9
-LINUX_KERNEL_HASH-4.19.148 = 3308c74a93ce6d4e495fde35d1351061240a895f76a288248c343713bd63555b
+LINUX_KERNEL_HASH-4.19.175 = ccee4b84ff2b16ad438e013bd211efea12f14e69f7cadb0e095fc89c781b2b25
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
