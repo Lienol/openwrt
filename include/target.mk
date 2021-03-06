8d251ed0ff6f449f89d3d5b81c199503d2a2ed18
Lienol <lawlienol@gmail.com>
2021-03-06 14:59:11 +0800

    target: add some default packages

--- include/target.mk
+++ include/target.mk
@@ -14,8 +14,9 @@ DEVICE_TYPE?=router
 
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd luci luci-compat wget-ssl curl \
-default-settings luci-app-wol luci-app-vlmcsd luci-app-ramfree \
+default-settings \
 luci-app-ddns ddns-scripts_aliyun ddns-scripts_dnspod \
+luci-app-upnp luci-app-wol luci-app-vlmcsd luci-app-ramfree \
 luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
