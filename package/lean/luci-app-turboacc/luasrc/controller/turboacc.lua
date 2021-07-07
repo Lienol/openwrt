aa68877078ae044fcecbab15e76acdea9cfa7245
Lienol <23146169+Lienol@users.noreply.github.com>
2021-07-08 00:39:48 +0800

    luci-app-turboacc: remove dns

--- package/lean/luci-app-turboacc/luasrc/controller/turboacc.lua
+++ package/lean/luci-app-turboacc/luasrc/controller/turboacc.lua
@@ -24,16 +24,11 @@ local function fullconenat_status()
 	return luci.sys.call("/etc/init.d/turboacc check_status fullconenat") == 0
 end
 
-local function dnscaching_status()
-	return luci.sys.call("/etc/init.d/turboacc check_status dns") == 0
-end
-
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 		fastpath_state = fastpath_status(),
 		fullconenat_state = fullconenat_status(),
-		bbr_state = bbr_status(),
-		dnscaching_state = dnscaching_status()
+		bbr_state = bbr_status()
 	})
 end
