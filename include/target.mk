8e334b3095d66e0074ec602628f48b9a37a7adc7
Lienol <23146169+Lienol@users.noreply.github.com>
2021-07-08 00:23:43 +0800

    target.mk: add `ddns-scripts-cloudflare` as DEFAULT_PACKAGES

--- include/target.mk
+++ include/target.mk
@@ -27,7 +27,7 @@ DEFAULT_PACKAGES:=\
 	urandom-seed \
 	luci luci-compat wget-ssl curl \
 	default-settings luci-app-upnp luci-app-wol luci-app-vlmcsd luci-app-ramfree \
-	luci-app-ddns ddns-scripts_aliyun ddns-scripts_dnspod \
+	luci-app-ddns ddns-scripts-cloudflare ddns-scripts_aliyun ddns-scripts_dnspod \
 	luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl \
 	urngd
 
