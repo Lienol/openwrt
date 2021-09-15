bb807d90684347b919e115b4322f9f31ea2d9094
Lienol <23146169+Lienol@users.noreply.github.com>
2021-09-15 17:31:59 +0800

    dnsmasq: Update to version 2.86

--- package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
+++ package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-[ "$(uci get dhcp.@dnsmasq[0].resolvfile)" = "/tmp/resolv.conf.auto" ] && {
-	uci set dhcp.@dnsmasq[0].resolvfile="/tmp/resolv.conf.d/resolv.conf.auto"
+[ "$(uci get dhcp.@dnsmasq[0].resolvfile)" = "/tmp/resolv.conf.d/resolv.conf.auto" ] && {
+	uci set dhcp.@dnsmasq[0].resolvfile="/tmp/resolv.conf.auto"
 	uci commit dhcp
 }
 
