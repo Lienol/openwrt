0eecfe2cb17b4b911140c64bfbf8252d44ca32cb
remittor <remittor@gmail.com>
2024-03-25 10:31:35 +0300

    mediatek: filogic: asus: Fix loading very large images (more 16 MiB)

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -190,7 +190,6 @@ define Device/asus_tuf-ax4200
   DEVICE_MODEL := TUF-AX4200
   DEVICE_DTS := mt7986a-asus-tuf-ax4200
   DEVICE_DTS_DIR := ../dts
-  DEVICE_DTS_LOADADDR := 0x47000000
   DEVICE_PACKAGES := kmod-usb3 kmod-mt7986-firmware mt7986-wo-firmware
   IMAGES := sysupgrade.bin
   KERNEL := kernel-bin | lzma | \
@@ -206,7 +205,6 @@ define Device/asus_tuf-ax6000
   DEVICE_MODEL := TUF-AX6000
   DEVICE_DTS := mt7986a-asus-tuf-ax6000
   DEVICE_DTS_DIR := ../dts
-  DEVICE_DTS_LOADADDR := 0x47000000
   DEVICE_PACKAGES := kmod-usb3 kmod-mt7986-firmware mt7986-wo-firmware
   IMAGES := sysupgrade.bin
   KERNEL := kernel-bin | lzma | \
