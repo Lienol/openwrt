259b2344f0e0322fa7c8d2366e551a61c86015cf
Lienol <23146169+Lienol@users.noreply.github.com>
2024-06-29 02:32:56 +0800

    wifi-scripts: set mac80211 script default config

--- package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+++ package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
@@ -167,6 +167,9 @@ detect_mac80211() {
 			dev_id="set wireless.radio${devidx}.macaddr=$(cat /sys/class/ieee80211/${dev}/macaddress)"
 		fi
 
+		ssid="OpenWrt"
+		[ "${mode_band}" = "5g" ] && ssid="${ssid}_5G"
+
 		uci -q batch <<-EOF
 			set wireless.radio${devidx}=wifi-device
 			set wireless.radio${devidx}.type=mac80211
@@ -181,7 +184,7 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
+			set wireless.default_radio${devidx}.ssid=${ssid}
 			set wireless.default_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
