f428d1df88e090e3436045b1de7d025e06d4cdb5
Lienol <23146169+Lienol@users.noreply.github.com>
2024-08-29 20:45:07 +0800

    qualcommax: update default packages

--- target/linux/qualcommax/ipq807x/target.mk
+++ target/linux/qualcommax/ipq807x/target.mk
@@ -1,6 +1,11 @@
 SUBTARGET:=ipq807x
 BOARDNAME:=Qualcomm Atheros IPQ807x
-DEFAULT_PACKAGES += kmod-phy-aquantia ath11k-firmware-ipq8074
+DEFAULT_PACKAGES += \
+	ath11k-firmware-ipq8074 nss-firmware-ipq8074 \
+	kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-clmapmgr kmod-qca-nss-drv-eogremgr kmod-qca-nss-drv-gre \
+	kmod-qca-nss-drv-igs kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-lag-mgr kmod-qca-nss-drv-map-t kmod-qca-mcs kmod-qca-nss-ecm \
+	kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-qca-nss-drv-pvxlanmgr kmod-qca-nss-drv-qdisc \
+	kmod-qca-nss-drv-tun6rd kmod-qca-nss-drv-tunipip6 kmod-qca-nss-drv-vlan-mgr kmod-qca-nss-drv-vxlanmgr qca-ssdk-shell
 
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ807x based boards.
