7542c823562ac200892b63f4f726e7cb05d5fcf5
Lienol <23146169+Lienol@users.noreply.github.com>
2024-08-30 18:44:09 +0800

    qualcommax: add NSS support

--- target/linux/qualcommax/ipq807x/target.mk
+++ target/linux/qualcommax/ipq807x/target.mk
@@ -1,6 +1,15 @@
 SUBTARGET:=ipq807x
 BOARDNAME:=Qualcomm Atheros IPQ807x
-DEFAULT_PACKAGES += kmod-phy-aquantia ath11k-firmware-ipq8074 nss-firmware-ipq807x
+DEFAULT_PACKAGES += \
+	ath11k-firmware-ipq8074 nss-firmware-ipq807x \
+	kmod-nss-ifb kmod-qca-nss-crypto \
+	kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-eogremgr kmod-qca-nss-drv-gre \
+	kmod-qca-nss-drv-igs kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-lag-mgr \
+	kmod-qca-nss-drv-map-t kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror kmod-qca-nss-drv-netlink \
+	kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-qca-nss-drv-qdisc \
+	kmod-qca-nss-drv-tun6rd kmod-qca-nss-drv-tunipip6 kmod-qca-nss-drv-vlan-mgr kmod-qca-nss-drv-vxlanmgr \
+	kmod-qca-mcs kmod-qca-nss-ecm \
+	qca-ssdk-shell
 
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ807x based boards.
