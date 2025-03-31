caa885f8433e987f622228ace46bcd3d3c15d08a
Yang Xiwen <forbidden405@outlook.com>
2025-03-31 08:48:23 +0800

    ipq60xx: add support for TP-Link EAP623-Outdoor HD v1
    Specifications:
    * SoC: Qualcomm IPQ6018 (64-bit Quad-core Arm Cortex-A53 @ 1800MHz)
    * Memory: 1 GiB
    * Serial Port: 3v3 TTL 115200n8
    * Wi-Fi: QCN9074 (4x4 5 GHz 802.11ax)
    * Wi-Fi: IPQ6018 (4x4 2.4 GHz 802.11b/g/n/ax)
    * Ethernet: RTL8211F (10/100/1GBASE-T)
    * Flash: ESMT F59D1G81MB (128 MiB)
    * LEDs: 1x Green Status (GPIO 37 Active High), 1x Yellow Status (GPIO 32
      Active High) and an LED global control GPIO (GPIO 36 Active High, set
      up by U-Boot)
    * Buttons: 1x Reset (GPIO 9 Active Low)
    
    Installation Instructions (Serial+TFTP):
    1. Solder 4 pin header to the pads near T32 and T31.
    2. Connect 3V3 TTL port to TX, RX, and GND, which are pad T31, T32 and
       the pad near T31 respectively. Be sure not to connect VCC and
       crossover TX and RX.
    3. Copy RAM firmware image
       openwrt-qualcommax-ipq60xx-tplink_eap623od-hd-v1-initramfs-uImage.itb
       to TFTP server root, available at 192.168.0.1.
    4. Connect PoE ethernet cable to the RJ45 port and hold Ctrl+B in the
       serial console (115200 baud) until autoboot is halted.
    5. Run the following commands in the U-boot prompt:
       # setenv serverip 192.168.0.1
       # setenv ipaddr 192.168.0.99
       # tftpboot 0x44000000 openwrt-qualcommax-ipq60xx-tplink_eap623od-hd-v1-initramfs-uImage.itb
       # bootm
       You may need to type Ctrl+C and Enter before running these commands
       to clear invisible characters from the buffer.
    6. Run the following command in a terminal to copy the sysupgrade image
       to be installed (check IP address):
       $ scp openwrt-qualcommax-ipq60xx-tplink_eap623od-hd-v1-squashfs-sysupgrade.bin root@192.168.1.1:/tmp/
    7. Activate the OpenWrt serial console and run the following commands:
       # cd /tmp
       # sysupgrade -n openwrt-qualcommax-ipq60xx-tplink_eap623od-hd-v1-squashfs-sysupgrade.bin
    8. The AP will reboot and OpenWrt will be successfully installed.
    
    Signed-off-by: Yang Xiwen <forbidden405@outlook.com>
    Link: https://github.com/openwrt/openwrt/pull/18389
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -122,7 +122,8 @@ platform_do_upgrade() {
 	zn,m2)
 		nand_do_upgrade "$1"
 		;;
-	tplink,eap610-outdoor)
+	tplink,eap610-outdoor|\
+	tplink,eap623od-hd-v1)
 		tplink_do_upgrade "$1"
 		;;
 	jdcloud,re-cs-02|\
