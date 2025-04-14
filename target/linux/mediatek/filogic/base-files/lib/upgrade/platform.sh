0bacbaf3c89325f44de5503ad47481ea4a4f3653
Christoph Krapp <achterin@gmail.com>
2025-04-14 20:47:56 +0200

    mediatek: filogic: add support for ASUS RT-AX52
    Hardware
    --------
    SOC:   MediaTek MT7981b
    RAM:   256MB DDR3
    FLASH: 128MB SPI-NAND (Winbond W25N01GV)
    WIFI:  Mediatek MT7981b DBDC 802.11ax 2.4/5 GHz
    ETH:   MediaTek MT7531 Switch
    UART:  3V3 115200 8N1 (Pinout silkscreened / Do not connect VCC)
    
    Installation
    -----------------------------------------------------------
    Vendor-UI Method
    -----------------------------------------------------------
    1. Download the OpenWrt initramfs.trx image.
    
    2. Connect the PC via LAN to one of the yellow router ports and wait
       until your PC to get a DHCP lease.
    
    3. Browse to http://192.168.50.1
    
    4. If your router is brand new, finish the setup process and log into
       the Web-UI.
    
    5. Navigate to Administration -> Firmware Upgrade and upload the
       downloaded OpenWrt image.
    
    6. Wait for OpenWrt to boot. Transfer the sysupgrade image to the device
       using scp and install using sysupgrade.
    
       $ sysupgrade -n <path-to-sysupgrade.bin>
    -----------------------------------------------------------
    TFTP Method
    -----------------------------------------------------------
    1. Download the OpenWrt initramfs image. Copy the image to a TFTP server
       reachable at 192.168.1.70/24. Rename the image to rtax52.bin.
    
    2. Connect the PC with TFTP server to the RT-AX52.
       Set a static ip on the ethernet interface of your PC.
         (ip address: 192.168.1.70, subnet mask:255.255.255.0)
      Conect to the serial console,
       interrupt the autoboot process by pressing '4' when prompted.
    
    3. Download & Boot the OpenWrt initramfs image.
    
       $ setenv ipaddr 192.168.1.1
       $ setenv serverip 192.168.1.70
       $ tftpboot 0x46000000 rtax52.bin
       $ bootm 0x46000000
    
    4. Wait for OpenWrt to boot. Transfer the sysupgrade image to the device
       using scp and install using sysupgrade.
    
       $ sysupgrade -n <path-to-sysupgrade.bin>
    ---------------------------------------------------------------------------
    Revert to stock firmware:
    1: Download the rt-ax52 firmware from ASUS official website. Save
       the firmware to tftp server directory and rename to RT-AX52.trx
    
    2: Connect the PC with TFTP server to the RT-AX52.
       Set a static ip on the ethernet interface of your PC.
         (ip address: 192.168.1.70, subnet mask:255.255.255.0)
    
    3: Conect to the serial console,  power on again,  interrupt the
       autoboot process by pressing '4' when prompted.
      $: ubi remove linux
      $: ubi remove jffs2
      $: ubi remove rootfs
      $: ubi remove rootfs_data
      $: ubi create linux 0x45fe000
      $: reset
    
      Then the dut will reboot,interrupt the autoboot process by
      pressing '2' when prompted.
           2: Load System code then write to Flash via TFTP.
           Warning!! Erase Linux in Flash then burn new one. Are you sure?(Y/N)
           $: enter y
      you will see the follow, type enter directly:
           Input device IP (192.168.1.1) ==:
           Input server IP (192.168.1.70) ==:
           Input Linux Kernel filename (RT-AX52.trx) ==:
    
    4: wait for the device run up
    
    Based on support for ASUS RT-AX52 by liudongdongdong7397
    and trx image generation by remittor
    
    Signed-off-by: Christoph Krapp <achterin@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -117,6 +117,7 @@ platform_do_upgrade() {
 		CI_ROOTPART="rootfs"
 		emmc_do_upgrade "$1"
 		;;
+	asus,rt-ax52|\
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
 	asus,tuf-ax6000)
@@ -243,6 +244,7 @@ platform_pre_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	asus,rt-ax52|\
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
 	asus,tuf-ax6000)
