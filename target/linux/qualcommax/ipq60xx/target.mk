acfe440a2a9f53cfd0e1e37b0b5decc78886d730
Lienol <23146169+Lienol@users.noreply.github.com>
2025-04-29 19:43:07 +0800

    qca-nss: update

--- target/linux/qualcommax/ipq60xx/target.mk
+++ target/linux/qualcommax/ipq60xx/target.mk
@@ -1,7 +1,7 @@
 SUBTARGET:=ipq60xx
 BOARDNAME:=Qualcomm Atheros IPQ60xx
 DEFAULT_PACKAGES += \
-	ath11k-firmware-ipq6018 nss-firmware-ipq6018 \
+	ath11k-firmware-ipq6018 nss-firmware-ipq60xx \
 	kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-clmapmgr kmod-qca-nss-drv-eogremgr kmod-qca-nss-drv-gre \
 	kmod-qca-nss-drv-igs kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-lag-mgr kmod-qca-nss-drv-map-t kmod-qca-mcs kmod-qca-nss-ecm \
 	kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-qca-nss-drv-pvxlanmgr kmod-qca-nss-drv-qdisc \
--- target/linux/qualcommax/ipq807x/target.mk
+++ target/linux/qualcommax/ipq807x/target.mk
@@ -1,7 +1,7 @@
 SUBTARGET:=ipq807x
 BOARDNAME:=Qualcomm Atheros IPQ807x
 DEFAULT_PACKAGES += \
-	ath11k-firmware-ipq8074 nss-firmware-ipq8074 \
+	ath11k-firmware-ipq8074 nss-firmware-ipq807x \
 	kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-clmapmgr kmod-qca-nss-drv-eogremgr kmod-qca-nss-drv-gre \
 	kmod-qca-nss-drv-igs kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-lag-mgr kmod-qca-nss-drv-map-t kmod-qca-mcs kmod-qca-nss-ecm \
 	kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-qca-nss-drv-pvxlanmgr kmod-qca-nss-drv-qdisc \
