4a79976154f2a2d65e7a591ff6015973a30a14be
Lienol <23146169+Lienol@users.noreply.github.com>
2025-10-16 19:11:05 +0800

    mediatek: add u-boot mod version for Redmi Router AX6000

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2050,6 +2050,20 @@ endif
 endef
 TARGET_DEVICES += xiaomi_redmi-router-ax6000-stock
 
+define Device/xiaomi_redmi-router-ax6000-mod
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_MODEL := Redmi Router AX6000 (Custom U-Boot layout)
+  DEVICE_DTS := mt7986a-xiaomi-redmi-router-ax6000-mod
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-leds-ws2812b kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += xiaomi_redmi-router-ax6000-mod
+
 define Device/xiaomi_redmi-router-ax6000-ubootmod
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := Redmi Router AX6000 (OpenWrt U-Boot layout)
