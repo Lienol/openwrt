7f2fe4fa8280e905989e9ef760c81436197cfddf
Andrew LaMarche <andrewjlamarche@gmail.com>
2026-02-23 01:26:31 +0000

    mediatek: add support for BananaPi BPi-R4 Pro 8X
    The BPi-R4 Pro 8X is a Wi-Fi 7 router board based on the MT7988A SoC.
    
    Specification
    -------------
    
    - SoC:		Mediatek MT7988A (4x Cortex-A73 @1.8GHz)
    - RAM:		8GB DDR4
    - Flash: 	256MB SPI-NAND, 8GB eMMC, microSD Slot
    - Switch:	MT7988A built-in switch, MxL86252C
    - PHYs:		2x AS21010P
    - Ports:	9 ports, 7 usable simultaneously
    	* 1x 1G RJ45 LAN
    	* 4x 2.5G RJ45 LAN
    	* 1x 10G RJ45/SFP Combo
    	* 1x 10G RJ45/SFP Combo
    - Buttons:	Reset, WPS
    - LEDs:		Power
    - USB:		On-board VIA VL822 USB3.2/USB2.0 hub
    	* 1x USB-A 2.0 connector
    	* 1x USB-A 3.2 connector
    	* 3x NGFF-KEYB
    - PCIe:
    	* 2x mini-PCIe slots with PCIe 3.0 2-lane interface
    	* 2x M.2 M-Key slots with PCIe 3.0 1-lane interface (switchable
    	  PCIe signal with M.2 B-Key)
    - Debug:	USB Type-C console port
    - Power:	USB Type-C PD 20V or DC barrel connector
    
    Note: This device comes without a MAC address programmed from the
    factory. The randomly generated MAC is stored in the U-Boot env on first
    boot and used persistently afterwards.
    
    Installation
    ------------
    
    Uncompress *sdcard.img.gz and write to microSD card, eg. using 'dd'. Use
    bootloader menu on the serial console to install SPI-NAND once installed
    to SPI-NAND you can used the bootloader menu to install to eMMC. See
    instructions for BananaPi R4 for details.
    
    Signed-off-by: Andrew LaMarche <andrewjlamarche@gmail.com>
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Co-Authored-By: Frank Wunderlich <frank-w@public-files.de>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -157,6 +157,7 @@ platform_do_upgrade() {
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	bananapi,bpi-r4-lite|\
+	bananapi,bpi-r4-pro-8x|\
 	bazis,ax3000wm|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
@@ -405,6 +406,7 @@ platform_check_image() {
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	bananapi,bpi-r4-lite|\
+	bananapi,bpi-r4-pro-8x|\
 	bazis,ax3000wm|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
@@ -483,6 +485,7 @@ platform_copy_config() {
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	bananapi,bpi-r4-lite|\
+	bananapi,bpi-r4-pro-8x|\
 	cmcc,rax3000m|\
 	gatonetworks,gdsp|\
 	mediatek,mt7988a-rfb)
