a8e4cf04bd91912af06c53a04500428bb66f5167
Stijn Segers <foss@volatilesystems.org>
2026-07-21 00:04:20 +0200

    realtek: use Realtek PSE MCU driver for GS1900-8HP A1
    Switch the GS1900-8HP A1 to the PSE driver that is being upstreamed.
    
    Signed-off-by: Stijn Segers <foss@volatilesystems.org>
    Link: https://github.com/openwrt/openwrt/pull/24339
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/realtek/image/rtl838x.mk
+++ target/linux/realtek/image/rtl838x.mk
@@ -393,7 +393,7 @@ define Device/zyxel_gs1900-8hp-a1
   DEVICE_VARIANT := A1
   ZYXEL_VERS := AAHI
   SUPPORTED_DEVICES += zyxel,gs1900-8hp-v1
-  DEVICE_PACKAGES += realtek-poe
+  DEVICE_PACKAGES += kmod-pse-realtek-mcu-uart
 endef
 TARGET_DEVICES += zyxel_gs1900-8hp-a1
 
