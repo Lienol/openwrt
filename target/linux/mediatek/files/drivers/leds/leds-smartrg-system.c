e38f5b8176fb32e62089819276d0cbeb80ad0fe1
Chad Monroe <chad@monroe.io>
2026-08-24 14:21:45 -0700

    mediatek: leds-smartrg: limit max_brightness to MCU range
    255 is outside the MCU's documented brightness range, which ends
    at 254, and the LED core clamps every brightness write, including
    trigger-driven ones, to max_brightness. Advertise max_brightness
    254 instead of LED_FULL.
    
    Signed-off-by: Chad Monroe <chad@monroe.io>
    Link: https://github.com/openwrt/openwrt/pull/24354
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/mediatek/files/drivers/leds/leds-smartrg-system.c
+++ target/linux/mediatek/files/drivers/leds/leds-smartrg-system.c
@@ -139,7 +139,8 @@ srg_led_init_led(struct srg_led_ctrl *sysled_ctrl, struct device_node *np)
 
 	led_cdev->name = of_get_property(np, "label", NULL) ? : np->name;
 	led_cdev->brightness = LED_OFF;
-	led_cdev->max_brightness = LED_FULL;
+	/* MCU documented brightness range ends at 254 */
+	led_cdev->max_brightness = 254;
 	led_cdev->brightness_set_blocking = srg_led_set_brightness;
 	led_cdev->blink_set = srg_led_set_pulse;
 
